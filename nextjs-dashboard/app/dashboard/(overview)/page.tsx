840c042f1c051de476725334fce23bb2ab0416c0
gatuati <estherwgatuati>
2026-05-20 11:26:48 +0300

    updated page

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -9,6 +9,7 @@ import { RevenueChartSkeleton,LatestInvoicesSkeleton,CardsSkeleton, } from '@/ap
 
 export default async function Page() {
   
+  
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
